a30d3208e7a853746723fe32396ceef4d627bd73
shahdmohammed <shahodmohammed2030@gmail.com>
2026-04-14 23:20:20 +0200

    Add start script to package.json and update main.js

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./src/database/connection.js"
 import { bootstrap } from "./app.controller.js"
+import cors from "cors";
+
+app.use(cors());
 
 dotenv.config()
 
@@ -12,6 +15,7 @@ app.use(express.json())
 connectDB()
 
 bootstrap(app)
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000
+  , () => {
   console.log("server running")
 })
